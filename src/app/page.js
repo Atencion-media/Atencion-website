fa8f82670027f48f1625e2c946aaa90dfd138a53
Shreyash <shreyash.22010681@viit.ac.in>
2024-05-08 22:20:33 +0530

    Resolved conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 import Navbar from "@/common/layout/Navbar";
 import CaseStudies from "@/pages/Home/CaseStudies";
 import Hero from "@/pages/Home/Hero";
-<<<<<<< HEAD
 import Slider1 from "@/pages/Home/Testimonials";
-=======
 import OurServices from "@/pages/Home/OurServices";
->>>>>>> 2148b7a549c64640a549e89f0eda17066f47ea1d
 import UnleaseCard from "@/pages/Home/UnleaseCard";
 import VideoPlayer from "@/pages/Home/VideoPlayer";
 
